27d375ba0e6b09079424a8df288e52e8f9026b50
NEVEROV iaroslav <beeguy74@gmail.com>
2024-12-08 14:02:48 +0100

    gitignore and test of secrets

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from os import getenv
 
 app = FastAPI()
+MY_KEY = getenv("MY_KEY")
 
 @app.get("/")
 def greet_json():
-    return {"Hello": "World!"}
+    return {"Hello": "World! My key is: " + MY_KEY}
 
