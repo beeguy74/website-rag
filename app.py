3426e6e86811e6355fb6905ed87d7c5cff7ff5b7
NEVEROV iaroslav <beeguy74@gmail.com>
2024-12-08 14:30:59 +0100

    triying embeddings

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from os import getenv
+from langchain_huggingface import HuggingFaceEmbeddings
 
 app = FastAPI()
 MY_KEY = getenv("MY_KEY")
+
+embeddings = HuggingFaceEmbeddings(model_name="jinaai/jina-embeddings-v2-small-en")
+
+
+@app.get("/embeddings")
+def get_embeddings():
+    test = embeddings.embed_query("Hello, world!")
+    return {
+        "embeddings": "This is the embeddings endpoint",
+        "test": test
+        }
 
 @app.get("/")
 def greet_json():
